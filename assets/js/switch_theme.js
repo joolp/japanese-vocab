1f14c936814556ce3f9cd7d420ee611ca6678c8e
joolp <88450061+joolp@users.noreply.github.com>
2025-04-04 13:47:42 +0000

    add style to links

--- assets/js/switch_theme.js
+++ assets/js/switch_theme.js
@@ -1,34 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('#switch-theme');
     const body = document.querySelector('body');
-    
-    if (localStorage.getItem('theme') == null)
+    const links = document.querySelectorAll('a');
+
+    const onloadTheme = localStorage.getItem('theme');
+
+    if (onloadTheme == null || onloadTheme == 'light')
     {
-        localStorage.setItem('theme', 'light');
+        setLight();
     }
-    if (localStorage.getItem('theme') == 'light')
+    else
     {
-        body.classList.remove('dark-mode');
-        button.classList.remove('dark-mode');
-    }
-    else if (localStorage.getItem('theme') == 'dark')
-    {
-        body.classList.add('dark-mode');
-        button.classList.add('dark-mode');
+        setDark();
     }
 
     button.addEventListener('click', () => {
         if (localStorage.getItem('theme') == 'dark')
         {
-            localStorage.setItem('theme', 'light');
-            body.classList.remove('dark-mode');
-            button.classList.remove('dark-mode');
+            setLight();
         }
-        else if (localStorage.getItem('theme') == 'light')
+        else
         {
-            localStorage.setItem('theme', 'dark');
-            body.classList.add('dark-mode');
-            button.classList.add('dark-mode');
+            setDark();
         }
     });
+
+    function setDark(){
+        localStorage.setItem('theme', 'dark');
+        
+        body.classList.add('dark-mode');
+        button.classList.add('dark-mode');
+
+        links.forEach((link) => {
+            link.classList.add('dark-mode');
+        });
+    }
+
+    function setLight(){
+        localStorage.setItem('theme', 'light');
+        
+        body.classList.remove('dark-mode');
+        button.classList.remove('dark-mode');
+
+        links.forEach((link) => {
+            link.classList.remove('dark-mode');
+        });
+    }
 });
