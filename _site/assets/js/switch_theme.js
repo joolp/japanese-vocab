cf1e63eb3efe3a27c89deefe987f88f91abecd69
joolp <88450061+joolp@users.noreply.github.com>
2025-04-04 09:36:48 +0000

    fix theme switching

--- _site/assets/js/switch_theme.js
+++ _site/assets/js/switch_theme.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('#switch-theme');
+    const body = document.querySelector('body');
+    
     if (localStorage.getItem('theme') == null)
     {
         localStorage.setItem('theme', 'light');
     }
+    if (localStorage.getItem('theme') == 'light')
+    {
+        body.classList.remove('dark-mode');
+        button.classList.remove('dark-mode');
+    }
+    else if (localStorage.getItem('theme') == 'dark')
+    {
+        body.classList.add('dark-mode');
+        button.classList.add('dark-mode');
+    }
 
-    const button = document.querySelector('#switch-theme');
-    const body = document.querySelector('body');
     button.addEventListener('click', () => {
         if (localStorage.getItem('theme') == 'dark')
         {
